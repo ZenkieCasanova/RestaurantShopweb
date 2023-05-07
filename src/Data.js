6aace5cf2a79bc0286cd48861a806c1155215298
ZenkieCasanova <130086552+ZenkieCasanova@users.noreply.github.com>
2023-05-07 18:09:27 +0800

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+let shopItemsData = [
+  {
+    id: "MEAL00001",
+    name: "Fried Chicken",
+    price: 120,
+    desc: "1 pc fried chicken with rice and tomato sauce.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "MEAL00002",
+    name: "Pancit Bihon",
+    price: 200,
+    desc: "Medium-sized Pansit Bihon. Good for 1-2 pax",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "MEAL00003",
+    name: "Chicken Inasal",
+    price: 147,
+    desc: "<p>3 pcs Chicken Inasal</p>",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "MEAL00004",
+    name: "Sinigang na Baboy",
+    price: 165,
+    desc: "Special Sinigang na Baboy",
+    img: "images/img-4.jpg",
+  },
+  {
+    id: "ITEM00001",
+    name: "Coca Cola 1.5L",
+    price: 70,
+    desc: "Coca Cola 1.5L",
+    img: "images/img-5.jpg",
+  },
+  {
+    id: "ITEM00002",
+    name: "Additional Rice",
+    price: 22,
+    desc: "1 pc extra rice",
+    img: "images/img-6.jpg",
+  },
+];
